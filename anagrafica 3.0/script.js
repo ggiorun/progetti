862246651483ba2dcf3bacb87416307832e4dba9
ggiorun <aivideos.tiktok06@gmail.com>
2025-11-17 16:18:12 +0100

    ciao

--- anagrafica 3.0/script.js
+++ anagrafica 3.0/script.js
@@ -26,7 +26,6 @@ function stampa() {
         }
     }
 
-
     let materie = [];
     let selected = document.getElementById("materie").selectedOptions;
 
@@ -34,7 +33,6 @@ function stampa() {
         let opzione = selected[i];
         materie.push(opzione.value);
     }
-
 
     let mezziPosseduti;
     if (mezzi.length > 0) {
@@ -85,21 +83,19 @@ function stampa() {
             cap: cap
         }
     };
-
-    document.getElementById("output").innerHTML = `
-      <h2>Dati Inseriti</h2>
-      <p><strong>Nome:</strong> ${persona.nome}</p>
-      <p><strong>Cognome:</strong> ${persona.cognome}</p>
-      <p><strong>Indirizzo:</strong> ${persona.indirizzo.via}</p>
-      <p><strong>Città:</strong> ${persona.indirizzo.citta}</p>
-      <p><strong>CAP:</strong> ${persona.indirizzo.cap}</p>
-      <p><strong>Età:</strong> ${persona.eta || "Non specificata"}</p>
-      <p><strong>Data di nascita:</strong> ${persona.dataNascita || "Non indicata"}</p>
-      <p><strong>Sesso:</strong> ${persona.sesso}</p>
-      <p><strong>Provincia:</strong> ${persona.provincia}</p>
-      <p><strong>Mezzi posseduti:</strong> ${persona.mezziPosseduti}</p>
-      <p><strong>Materie preferite:</strong> ${persona.materiePreferite}</p>
-      <p><strong>Generazione:</strong> ${persona.generazione}</p>
-    `;
+    document.getElementById("output").innerHTML = 
+    "<h2>Dati Inseriti</h2>" +
+    "<p><strong>Nome:</strong> " + persona.nome + "</p>" +
+    "<p><strong>Cognome:</strong> " + persona.cognome + "</p>" +
+    "<p><strong>Indirizzo:</strong> " + persona.indirizzo.via + "</p>" +
+    "<p><strong>Città:</strong> " + persona.indirizzo.citta + "</p>" +
+    "<p><strong>CAP:</strong> " + persona.indirizzo.cap + "</p>" +
+    "<p><strong>Età:</strong> " + (persona.eta || "Non specificata") + "</p>" +
+    "<p><strong>Data di nascita:</strong> " + (persona.dataNascita || "Non indicata") + "</p>" +
+    "<p><strong>Sesso:</strong> " + persona.sesso + "</p>" +
+    "<p><strong>Provincia:</strong> " + persona.provincia + "</p>" +
+    "<p><strong>Mezzi posseduti:</strong> " + persona.mezziPosseduti + "</p>" +
+    "<p><strong>Materie preferite:</strong> " + persona.materiePreferite + "</p>" +
+    "<p><strong>Generazione:</strong> " + persona.generazione + "</p>";
 }
 
